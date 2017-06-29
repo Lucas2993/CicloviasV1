d66e8f2b716340955cac82dbd75be358b5d8878b
emanuel mallon <emanuelsm18@gmail.com>
2017-06-29 14:36:26 -0300

    agregando cambios en los spinner y LineString_longLat para solucionar el problema de que se modifique el orden de los puntos de los recorridos

---
+++ LineString_LongLat.sql
@@ -0,0 +1,5 @@
+﻿-- NOTA: Si laravel devuelve al revez las coordenadas en algún LineString
+ALTER TABLE trips
+  ALTER COLUMN geom TYPE geography(LineString,4326)
+  USING ST_FlipCoordinates(
+    geom::geometry)::geography(LineString,4326);
